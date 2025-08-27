4e6273e34a6a453f81b3e8579cf4841a8c240ac2
Tanim Ahamed <tanimislamt@gmail.com>
2025-08-27 16:33:23 +0600

    Alhamdulillah, Assignment 5 done

--- script/index.js
+++ script/index.js
@@ -3,7 +3,7 @@ function getElement(id) {
     return element;
 }
 
-// heart icon function 
+
 const hearts = document.getElementsByClassName('heart-icon')
 
 for (let heart of hearts) {
@@ -14,32 +14,39 @@ for (let heart of hearts) {
             getElement('heart-count').innerText = currentHeart;
         })
 }
-
-// call btn function 
-
-getElement('card-1').addEventListener('click', function (e) {
+getElement('card-section-container').addEventListener('click', function (e) {
     const coin = Number(getElement('total-coin').innerText);
+
     if (e.target.className.includes('call-btn')) {
+        const callBtn = e.target;
+        console.log(callBtn);
+        const localTime = new Date().toLocaleTimeString();
+        console.log(localTime);
+        // console.log(callBtn);
+        const cardTitle = callBtn.parentNode.parentNode.children[1].innerText;
+        // console.log(cardTitle)
+        const cardNumber = callBtn.parentNode.parentNode.children[3].innerText;
+        // console.log(cardNumber);
         if (coin >= 20) {
             const currentCoin = coin - 20;
             getElement('total-coin').innerText = currentCoin;
-            alert('Calling National Emergency 999....');
+            alert("Calling"+ cardTitle + cardNumber + "....");
             const historyContainer = getElement('history-container');
             const createHistory = document.createElement('div')
             createHistory.innerHTML = `
-             <div class="bg-[#FAFAFA] rounded-[8px] p-[16px] flex justify-between items-center">
+              <div class="bg-[#FAFAFA] rounded-[8px] p-[16px] flex justify-between items-center mb-[10px]">
                         <div>
-                            <h1 class="font-semibold text-[18px]">Fire Service Number</h1>
-                            <h1 class="text-[#5C5C5C] text-[18px]">999</h1>
+                            <h1 class="font-semibold text-[18px]">${cardTitle}</h1>
+                            <h1 class="text-[#5C5C5C] text-[18px]">${cardNumber}</h1>
                         </div>
                         <div>
-                            <p>11:36:58 AM</p>
+                            <p>${localTime}</p>
                         </div>
                     </div>
             `
             historyContainer.append(createHistory);
         } else {
-            alert('আপনার পর্যাপ্ত কয়েন নেই, কল করতে কমপক্ষে ২০ কয়েন লাগবে ।');
+            alert("You don't have enough coins, you need at least 20 coins to make a call.");
         }
     }
 });
@@ -49,3 +56,32 @@ getElement('clear-btn').addEventListener('click', function () {
     const historyContainer = getElement('history-container');
     historyContainer.innerHTML = '';
 })
+
+
+//copy function
+
+getElement('card-section-container').addEventListener('click', function (e) {
+    if (e.target.className.includes('copy-btn')) {
+        const copyBtn = e.target;
+        const cardNumber = copyBtn.parentNode.parentNode.children[3].innerText;
+        navigator.clipboard.writeText(cardNumber);
+        alert('The number has been copied: ' + cardNumber);
+        const copyCount = getElement('copy-count').innerText;
+        const totalCopyCount = Number(copyCount) + 1
+        getElement('copy-count').innerText = totalCopyCount;
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
